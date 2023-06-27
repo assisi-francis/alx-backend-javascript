0373abcb09488bd79399bf18ad44597e616222cf
assisi-francis <doncyco123@gmail.com>
2023-06-27 11:16:44 +0100

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,5 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
   return array.map(value => appendString + value);
 }
