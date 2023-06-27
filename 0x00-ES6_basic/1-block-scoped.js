9a5d3cb2eb24be12daa8b732f7305c4de5e8373f
assisi-francis <doncyco123@gmail.com>
2023-06-27 10:31:00 +0100

    modify task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
-/* eslint-disabled-line */
-/* //eslint-disabled-line no-va */
-
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
 
   if (trueOrFalse) {
-    const task = true; //eslint-disabled-line
-    const task2 = false; //eslint-disabled-line
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
