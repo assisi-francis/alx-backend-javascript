e66d9d9f72bed310185331dfee15ef2e0cf10c3f
assisi-francis <doncyco123@gmail.com>
2023-06-27 12:47:58 +0100

    task 100

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,38 +1,8 @@
-/* iterator */
 export default function createIteratorObject(report) {
-  const departments = Object.entries(report.allEmployees).filter(([_, employees]) => employees.length > 0);
+  const myarray = [];
+  for (const item of Object.values(report.allEmployees)) {
+    myarray.push(...item);
+  }
 
-  let currentDepartmentIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  const iterator = {
-    next() {
-      if (currentDepartmentIndex >= departments.length) {
-        return { done: true };
-      }
-
-      const [departmentName, employees] = departments[currentDepartmentIndex];
-
-      if (currentEmployeeIndex < employees.length) {
-        const employee = employees[currentEmployeeIndex];
-        currentEmployeeIndex++;
-
-        return {
-          value: employee,
-          done: false,
-          department: departmentName,
-        };
-      }
-
-      currentDepartmentIndex++;
-      currentEmployeeIndex = 0;
-
-      return this.next();
-    },
-    [Symbol.iterator]() {
-      return this;
-    },
-  };
-
-  return iterator;
+  return myarray;
 }
