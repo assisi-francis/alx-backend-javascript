7f5a664fea1b437ae9e5e5becda801720712ae77
assisi-francis <doncyco123@gmail.com>
2023-06-28 12:43:44 +0100

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+/* promise */
+import { uploadPhoto, createUser } from "./utils.js";
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photoResponse, userResponse] = results;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch((error) => {
+      console.log("Signup system offline");
+    });
+}
+
+export default handleProfileSignup;
