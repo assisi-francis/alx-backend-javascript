9dcacecbc76047c63c2781fca6251c466815e2a6
assisi-francis <doncyco123@gmail.com>
2023-06-28 15:23:36 +0100

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,13 +2,19 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  Promise.allSettled([uploadPhoto(), createUser()])
     .then((results) => {
-      const [photoResponse, userResponse] = results;
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    })
-    .catch((error) => {
-      console.log('Signup system offline');
+      const fulfilledResponses = results.filter((result) => result.status === 'fulfilled');
+      const rejectedResponses = results.filter((result) => result.status === 'rejected');
+
+      if (fulfilledResponses.length > 0) {
+        const userResponse = fulfilledResponses[1].value;
+        console.log(`Body ${userResponse.firstName} ${userResponse.lastName}`);
+      }
+
+      if (rejectedResponses.length > 0) {
+        console.log('Signup system offline');
+      }
     });
 }
 
