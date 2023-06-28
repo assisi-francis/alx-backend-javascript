ec4f65ab71eef6227606bae68a333503090cc91c
assisi-francis <doncyco123@gmail.com>
2023-06-28 10:18:02 +0100

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,5 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Perform asynchronous operations here, such as making an API request
-    // Once the operation is complete, call either resolve() or reject() based on the result
-
-    // Example of resolving the promise with a value
-    resolve("API response data");
-
-    // Example of rejecting the promise with an error
-    // reject("API request failed");
-  });
+  return new Promise((resolve, reject) => {})
 }
 
 export default getResponseFromAPI;
