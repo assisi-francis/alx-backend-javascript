15e44a6a4ac726ff5363729c773a975172167ee5
assisi-francis <doncyco123@gmail.com>
2023-06-30 20:54:12 +0100

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 class Building {
   constructor(sqft) {
-    if (new.target === Building) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
