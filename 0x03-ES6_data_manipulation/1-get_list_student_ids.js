2fbfa367078a08b3b09340c89ddecea1f7df87c4
assisi-francis <doncyco123@gmail.com>
2023-07-03 22:18:18 +0100

    task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+function getListStudentIds(studentList) {
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
+
+  return studentList.map(student => student.id);
+}
+export default getListStudentIds;
