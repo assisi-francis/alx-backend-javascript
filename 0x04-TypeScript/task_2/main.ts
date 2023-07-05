9468c3544fb35c0036f30182030f52fbbcd8cbb6
assisi-francis <doncyco123@gmail.com>
2023-07-05 19:57:23 +0100

    task 6

--- 0x04-TypeScript/task_2/main.ts
+++ 0x04-TypeScript/task_2/main.ts
@@ -46,3 +46,14 @@ function createEmployee(salary: number | string): Director | Teacher {
   }
 }
 
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
