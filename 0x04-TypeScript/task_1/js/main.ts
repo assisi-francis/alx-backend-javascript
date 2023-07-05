3671d5cf72c4bb8c2f9d993385b78bb14234dfdc
assisi-francis <doncyco123@gmail.com>
2023-07-05 19:46:53 +0100

    add

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,45 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  const initial = firstName.charAt(0).toUpperCase();
+  return `${initial}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClassImpl implements StudentClass {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const StudentClass: StudentClassConstructor = StudentClassImpl;
+
