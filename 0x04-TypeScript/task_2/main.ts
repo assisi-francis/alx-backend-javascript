defcf7b2031e67e9ce68f4460fa48533e22300ab
assisi-francis <doncyco123@gmail.com>
2023-07-05 19:59:57 +0100

    task 7

--- 0x04-TypeScript/task_2/main.ts
+++ 0x04-TypeScript/task_2/main.ts
@@ -57,3 +57,15 @@ function executeWork(employee: Director | Teacher) {
     console.log(employee.workTeacherTasks());
   }
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    throw new Error('Invalid subject');
+  }
+}
