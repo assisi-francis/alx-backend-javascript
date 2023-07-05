382b91021aba06fccef99a6dc1c7675e0ef484be
assisi-francis <doncyco123@gmail.com>
2023-07-05 13:51:08 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,8 @@ function cleanSet(set, startString) {
   if (startString === undefined || startString.length === 0) {
     return '';
   }
-  return [...set]
-    .filter((str) => (str !== undefined ? str.startsWith(string) : ''))
-    .map((str) => (str !== undefined ? str.slice(string.length) : ''))
-    .join('-');
+  const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
+  const cleanedString = filteredValues.map(value => value.substring(startString.length)).join('-');
+  return cleanedString;
 }
 export default cleanSet;
